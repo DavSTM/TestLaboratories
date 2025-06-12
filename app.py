bfe076c5c12b0550ac9b1432c080f55d01f4ed82
DavSTM <dstst77@gmail.com>
2025-06-12 13:24:39 +0500

    orders_acknowledge

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 from flask import Flask
 from handlers.check_role import role_bp
-from handlers.acknowledge import ack_bp
+from handlers.orders_acknowledge import ack_bp
 
 app = Flask(__name__)
 app.register_blueprint(role_bp)
--- handlers/orders_acknowledge.py
+++ handlers/orders_acknowledge.py
@@ -3,8 +3,8 @@ from airtable_client import get_record, update_record, delete_record
 
 ack_bp = Blueprint("ack", __name__)
 
-@ack_bp.route("/acknowledge", methods=["POST"])
-def acknowledge():
+@ack_bp.route("/orders_acknowledge", methods=["POST"])
+def orders_acknowledge():
     data = request.json
     temp_id = data.get("recordId")  # ID записи, только что добавленной формой
 
@@ -14,11 +14,10 @@ def acknowledge():
     temp_rec = get_record("Журнал распоряжений", temp_id)
     fields = temp_rec.get("fields", {})
     base_id = fields.get("Переданный ID")
-    user = fields.get("Created By", [{}])[0]
+    user = fields.get("Распоряжение выдал", [{}])
     user_email = user.get("email")
-
     if not base_id or not user_email:
-        return Response("❌ Отсутствует email или переданный ID", status=400)
+        return Response(f"❌ Отсутствует email {user} или переданный ID", status=400)
 
     base_rec = get_record("Журнал распоряжений", base_id)
     base_fields = base_rec.get("fields", {})
@@ -26,21 +25,29 @@ def acknowledge():
     signatures = base_fields.get("Подписи", [])
 
     # Проверка: email отправителя есть в "Исполнителях"
-    match = False
+    match = []
     for person_id in performers_ids:
         person = get_record("Персонал", person_id)
         if person.get("fields", {}).get("Электронная почта") == user_email:
-            match = True
-            break
+            user_ava = person.get("fields", {}).get("User")
+            if user_ava:
+                match.append(user_ava)
+
 
     if match:
-        if user_email not in signatures:
-            signatures.append(user_email)
-            update_record("Журнал распоряжений", base_id, {
+        for perf in match:
+            if user_email not in [u.get("email") for u in signatures]:
+                signatures.append(perf)
+
+        update_record(
+            table="Журнал распоряжений",
+            record_id=base_id,
+            fields={
                 "Подписи": signatures
-            })
+            }
+        )
 
     # Удаляем временную запись, в любом случае
-    delete_record("Журнал распоряжений", temp_id)
+    # delete_record("Журнал распоряжений", temp_id)
 
-    return "✅ Обработка завершена"
+    return f"✅ Обработка {match} завершена"
