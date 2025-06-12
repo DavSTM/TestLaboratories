671896b829312db99bf811617e9f999fec4d2f21
DavSTM <dstst77@gmail.com>
2025-06-12 11:57:40 +0500

    Add acknowledge 2

--- app.py
+++ app.py
@@ -1,8 +1,10 @@
 from flask import Flask
 from handlers.check_role import role_bp
+from handlers.acknowledge import ack_bp
 
 app = Flask(__name__)
 app.register_blueprint(role_bp)
+app.register_blueprint(ack_bp)
 
 @app.route("/")
 def home():
